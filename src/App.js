582bcff7ad9df601deb516f254fe60357bceb616
reduktorius <reduktorius@gmail.com>
2020-05-15 11:35:39 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      input: "",
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = (event) => {
+    console.log('click')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/* <FaceRecognition /> */}
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Rank() {
+    return (
+        <div>
+            <div className="f3">
+                {'User rank is...'}
+            </div>
+            <div className="f1">
+                {'#9001.'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
