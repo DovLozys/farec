11209b479de6b85d156144e0da95e81350952f2e
reduktorius <reduktorius@gmail.com>
2020-05-19 12:20:17 +0100

    256-258 adding face detection box, sign in/register forms, routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 const app = new Clarifai.App({
   apiKey: '7ac1103d8c0a4180b526c103b3892764'
@@ -29,8 +31,29 @@ class App extends React.Component {
     super()
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
+  }
+
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById("inputimage")
+    const width = Number(image.width)
+    const height = Number(image.height)
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -39,25 +62,38 @@ class App extends React.Component {
 
   onButtonSubmit = (event) => {
     this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        console.log(err)
-      }
-    );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err))
+  }
+
+  onRouteChange = (dest) => {
+    if (dest === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (dest === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: dest})
   }
 
   render() {
+    const { isSignedIn, route, box, imageUrl } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particleOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home' ? 
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+        : (route === 'signin'
+          ? <SignIn onRouteChange={this.onRouteChange}/>
+          : <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
       </div>
     )
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function Navigation() {
-    return (
+function Navigation({onRouteChange, isSignedIn}) {
+    if (isSignedIn) {
+        return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim underline pa3 pointer'>Sign out</p>
-        </nav>
-    )
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim underline pa3 pointer'>Sign out</p>
+        </nav>)
+    } else {
+        return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim underline pa3 pointer'>Sign in</p>
+            <p onClick={() => onRouteChange('register')} className='f3 link dim underline pa3 pointer'>Register</p>
+        </nav>)
+    }
 }
 
 export default Navigation;
